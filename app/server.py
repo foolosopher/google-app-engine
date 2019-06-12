71ec09903dc92a3d2e0146e0bf84a379d4e2dafe
Artem <artem@MacBook-Pro-2.local>
2019-06-12 19:09:01 +0300

    actionable model link

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai.vision import *
 
 
-model_file_url = 'https://downloader.disk.yandex.ru/disk/0992b335d85847417957eae3bd3b7c7eb915c40fdc4b05e301730f9a0b0cf576/5d0070a2/H3Zf5VVVkY_-gozRn-nKUkD1bGu18eSM8b4QmdFnzXACHyMZTUo2PXBGNg4RxF9rDcscxd58fPcXxTO5TB10ew%3D%3D?uid=0&filename=stage-2-2.pth&disposition=attachment&hash=1xMUA/ngPhpe%2BAqEVmnl0j60CTE88db33zapHBi7DMK36ZQieMLcmS8Wuvj0ffgWq/J6bpmRyOJonT3VoXnDag%3D%3D%3A&limit=0&content_type=application%2Foctet-stream&fsize=262136576&hid=56b095cb7be6ea776f0de7eec052a879&media_type=data&tknv=v2'
+model_file_url = 'https://getfile.dokpub.com/yandex/get/https://yadi.sk/d/HtD-7-S803mvCw'
 model_file_name = 'model'
 classes=['Abyssinian','Bengal','Birman','Bombay','British_Shorthair','Egyptian_Mau','Maine_Coon','Persian','Ragdoll','Russian_Blue','Siamese','Sphynx','american_bulldog','american_pit_bull_terrier','basset_hound','beagle','boxer','chihuahua','english_cocker_spaniel','english_setter','german_shorthaired','great_pyrenees','havanese','japanese_chin','keeshond','leonberger','miniature_pinscher','newfoundland','pomeranian','pug','saint_bernard','samoyed','scottish_terrier','shiba_inu','staffordshire_bull_terrier','wheaten_terrier','yorkshire_terrier']
 path = Path(__file__).parent
